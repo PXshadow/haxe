4e7411b4fbceb78850cce7cf609c9abc919371f4
Joey <9813078+PXshadow@users.noreply.github.com>
2026-07-30 17:45:33 +0200

    add Go define for missing spots such as 0 over null, preventing skipped on non nullable arguments

--- tests/unit/src/unit/TestType.hx
+++ tests/unit/src/unit/TestType.hx
@@ -253,7 +253,7 @@ class TestType extends Test {
  		eq("foo0", f());
 
 		var foo = function(bar = 2) { return bar; };
-		#if flash // Cannot skip not-nullable argument
+		#if (flash || go) // Cannot skip not-nullable argument
 		t(typeError(foo.bind(_)));
 		#else
 		var l = foo.bind(_);
